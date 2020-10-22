c20286c7bccb13f25e53e171af1a3fe1f549d5af
Quentin-Loïc Prieur <quentinloicp@gmail.com>
2020-10-22 17:33:24 +0200

    init

---
+++ Assets/ConnectionServer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using UnityEngine;
+
+public class ConnectionServer : MonoBehaviour
+{
+    [SerializeField] private int port = 8888;
+    [SerializeField] private string ip = "localhost";
+    private TcpClient tcpClient;
+    private KartController controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<KartController>();
+        tcpClient = new TcpClient(ip, port);
+        if (tcpClient.Connected)
+        {
+            Debug.Log($"Connection to: ${ip} on port : ${port}");
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var buffer = new MemoryStream();
+        buffer.SetLength(1024);
+
+        var position = controller.GetPosition();
+        var bytesX = BitConverter.GetBytes(position.x);
+        buffer.Write(bytesX, 0, bytesX.Length);
+
+        var stream = tcpClient.GetStream();
+        stream.BeginWrite(buffer.ToArray(), 0, buffer.ToArray().Length,EndSend,buffer);
+    }
+
+    void EndSend(IAsyncResult result)
+    {
+        Debug.Log("data send");
+    }
+}
---
+++ Assets/KartController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KartController : MonoBehaviour
+{
+    [SerializeField] private Vector2 _position = new Vector2();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _position = new Vector2(Input.GetAxis("Vertical"),Input.GetAxis("Horizontal"));
+    }
+
+    public Vector2 GetPosition()
+    {
+        return _position;
+    }
+}
