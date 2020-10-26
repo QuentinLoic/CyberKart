33bc4f2b4b982cecbd9baab890100ecf553972de
Max <maximeld19@gmail.com>
2020-10-26 15:43:27 +0100

    Added new Packet system

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkManager : MonoBehaviour
+{
+    public static NetworkManager instance;
+
+    [SerializeField] private int targetFrameRate = 30;
+    [SerializeField] private int refreshTime = 10;
+    [SerializeField] private int connectionTimeOut = 2;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Debug.Log("Instance already exists, destroying object!");
+            Destroy(this);
+        }
+    }
+
+    private void Start()
+    {
+        
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = targetFrameRate;
+        Debug.Log($"Set FrameRate to {targetFrameRate}");
+
+        
+        StartCoroutine(ConnnectClient());
+    }
+
+    private void OnApplicationQuit()
+    {
+        Client.instance.Disconnect();
+    }
+
+    private IEnumerator ConnnectClient()
+    {
+        while (!Client.instance.IsConnected)
+        {
+            Debug.Log($"Start connect to server");
+            Client.instance.ConnectToServer();
+            yield return new WaitForSeconds(connectionTimeOut);
+
+            if (Client.instance.IsConnected)
+            {
+                Client.instance.tcp.onDisconnect.AddListener(OnTcpDisconnected);
+                break;
+            }
+
+            Debug.Log($"Waiting {refreshTime}s before new connection");
+
+            yield return new WaitForSeconds(refreshTime);
+        }
+        
+    }
+
+    private void OnTcpDisconnected()
+    {
+        StartCoroutine(ConnnectClient());
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Vector2 _position = new Vector2();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        SendInputToServer();
+    }
+
+    /// <summary>Sends player input to the server.</summary>
+    private void SendInputToServer()
+    {
+        _position = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        ClientSend.PlayerMovement(_position);
+    }
+
+    public Vector2 GetPosition()
+    {
+        return _position;
+    }
+}
